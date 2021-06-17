5035da1e875693e64f396a71f761373627cc449a
gcie <tao24.k@gmail.com>
2021-06-17 10:36:35 +0200

    update segment script model path

--- scripts/segment.sh
+++ scripts/segment.sh
@@ -8,8 +8,8 @@ for VS in 100 ; do
         /pio/data/zerospeech2021/quantized/LibriSpeech/${TRAINSET}/quantized_outputs.txt \
         /pio/data/zerospeech2021/quantized/LibriSpeech/${TESTSET}/quantized_outputs.txt \
         $VS \
-        /pio/scratch/1/i290956/zs2021/simi/segmentations/${TRAINSET}_${TESTSET}_vs${VS}_a$1_test \
-        --sentencepiece_prefix=/pio/scratch/1/i290956/zs2021/simi/sentencepieces/${TRAINSET}_vs${VS} \
+        /pio/scratch/1/i290956/zs2021/simi/models/segmentations/${TRAINSET}_${TESTSET}_vs${VS}_a$1_test \
+        --sentencepiece_prefix=/pio/scratch/1/i290956/zs2021/simi/models/sentencepieces/${TRAINSET}_vs${VS} \
         --segmentation_output_format=csv \
         --viterbi \
         --alpha $1 \
