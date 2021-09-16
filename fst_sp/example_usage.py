4a0ce15fb6cde664c17b1919e57f27c0efd03b15
gcie <tao24.k@gmail.com>
2021-09-16 08:56:15 +0200

    fst_sp initial interface

---
+++ fst_sp/example_usage.py
@@ -0,0 +1,36 @@
+from fst_sp import SentencePieceModel, SentencePieceTrainer
+
+
+# EXAMPLE 1
+
+# list of strings
+string_data = list(line.strip() for line in open('./botchan.txt', 'r', encoding='utf8'))
+# train model
+model = SentencePieceTrainer.train(string_data)
+# example sentence (string)
+sentence = 'I saw a girl with a telescope.'
+# segmenting the sentence
+encoding = model.encode(sentence)
+
+# assert encoding == ['I', ' saw', ' a', ' girl', ' with', ' a', ' ', 'te', 'le', 's', 'c', 'o', 'pe', '.']
+
+
+# EXAMPLE 2
+
+# list of lists of ints
+int_data = list(map(lambda s: list(map(ord, s)), string_data))
+# train model
+model = SentencePieceTrainer.train(int_data)
+# example sentence (list of ints)
+sentence = list(map(ord, 'I saw a girl with a telescope.'))
+# segmenting the sentence
+encoding = model.encode(sentence)
+
+# assert encoding == [[73], [32, 115, 97, 119], [32, 97], [32, 103, 105, 114, 108]...
+
+
+# EXAMPLE 3
+path = '/some/path/on/disk'
+model.save(path)
+model2 = SentencePieceModel(prefix=path)
+assert model2.encode(sentence) == encoding
